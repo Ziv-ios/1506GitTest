030cc8b640547475a7ff5ff0cf7a55d9626bffc8
tarena <tarena@Ziv-Mac.local>
2015-10-08 14:02:35 +0800

    log01

---
+++ Demo02-Git/Demo02-Git/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Demo02-Git
+//
+//  Created by tarena on 15/10/8.
+//  Copyright (c) 2015年 my. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    //jfewjufjfdfijidi
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
